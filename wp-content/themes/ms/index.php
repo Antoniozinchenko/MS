d0c1631dd442dc4e1c48df343e8e804d1fe041e1
Mentikora <notender_boy@ukr.net>
2015-11-03 18:18:17 +0200

    fx2

--- wp-content/themes/ms/index.php
+++ wp-content/themes/ms/index.php
@@ -762,11 +762,11 @@
                             <svg class="svg-graphic" width="245" height="250" viewBox="0 0 245 250" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
                                 <g>
                                     <clipPath id="hex-mask_worker">
-                                        <polygon points="123,0 0,67 0,183 123,250 250,183 245,67 123,0" />
+                                        <polygon points="123,0 0,67 0,183 123,250 245,183 245,67 123,0" />
                                     </clipPath>
                                 </g>
                                 <a xlink:href="javascript:void(0);">
-                                    <polygon fill="#fff" points="123,0 0,67 0,183 123,250 250,183 245,67 123,0" transform="" />
+                                    <polygon fill="#fff" points="123,0 0,67 0,183 123,250 245,183 245,67 123,0" transform="" />
                                     <image clip-path="url(#hex-mask_worker)" height="100%" width="100%" xlink:href="<?php echo $pwd ?>img/sviatoslav_lucushun.png" preserveAspectRatio="xMidYMin slice" />
                                     <p class="worker_pre">
                                         <span class="name">
@@ -795,11 +795,11 @@
                             <svg class="svg-graphic" width="245" height="250" viewBox="0 0 245 250" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
                                 <g>
                                     <clipPath id="hex-mask_worker">
-                                        <polygon points="123,0 0,67 0,183 123,250 250,183 245,67 123,0" />
+                                        <polygon points="123,0 0,67 0,183 123,250 245,183 245,67 123,0" />
                                     </clipPath>
                                 </g>
                                 <a xlink:href="javascript:void(0);">
-                                    <polygon fill="#fff" points="123,0 0,67 0,183 123,250 250,183 245,67 123,0" transform="" />
+                                    <polygon fill="#fff" points="123,0 0,67 0,183 123,250 245,183 245,67 123,0" transform="" />
                                     <image clip-path="url(#hex-mask_worker)" height="100%" width="100%" xlink:href="<?php echo $pwd ?>img/irina_romanuk.png" preserveAspectRatio="xMidYMin slice" />
                                     <p class="worker_pre">
                                         <span class="name">
@@ -829,11 +829,11 @@
                             <svg class="svg-graphic" width="245" height="250" viewBox="0 0 245 250" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1">
                                 <g>
                                     <clipPath id="hex-mask_worker">
-                                        <polygon points="123,0 0,67 0,183 123,250 250,183 245,67 123,0" />
+                                        <polygon points="123,0 0,67 0,183 123,250 245,183 245,67 123,0" />
                                     </clipPath>
                                 </g>
                                 <a xlink:href="javascript:void(0);">
-                                    <polygon fill="#fff" points="123,0 0,67 0,183 123,250 250,183 245,67 123,0" transform="" />
+                                    <polygon fill="#fff" points="123,0 0,67 0,183 123,250 245,183 245,67 123,0" transform="" />
                                     <image clip-path="url(#hex-mask_worker)" height="100%" width="100%" xlink:href="<?php echo $pwd ?>img/galina_nayda.png" preserveAspectRatio="xMidYMin slice" />
                                     <p class="worker_pre">
                                         <span class="name">
